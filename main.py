3918a6f09e7a8a4fa2a58a7daa1ee8fdc3adda83
chenyilun <joshua@joshuacyl.work>
2023-04-27 17:48:11 +0800

    U add some short param in main

--- main.py
+++ main.py
@@ -18,40 +18,40 @@ if __name__ == '__main__':
     subprasers.add_parser("reset", help="reset users' traffic. Normally used by crontab.")
     subprasers.add_parser("list", help="print all users' info")
     add = subprasers.add_parser("add", help="add new user")
-    add.add_argument("--monthly-traffic", type=int, default=100,
+    add.add_argument("-t", "--monthly-traffic", type=int, default=100,
                      help="monthly traffic set for this user (GB). Using 100 by default.")
     delete = subprasers.add_parser("del", help="delete user from ss-manager")
-    delete.add_argument("--port", type=str, required=True,
+    delete.add_argument("-p", "--port", type=str, required=True,
                         help="indicate the port of user to remove")
-    subscription = subprasers.add_parser("sub", help="print user's ")
-    subscription.add_argument("--port", type=str, required=True,
+    subscription = subprasers.add_parser("sub", help="print user's subscription")
+    subscription.add_argument("-p", "--port", type=str, required=True,
                               help="indicate the port of user to print subscription")
     parser.add_argument("-ss", "--ss-server", type=str, default="localhost",
                         help="ss-server address or domain using to generate subscription url.")
     parser.add_argument("-se", "--ss-encryption", type=str, default="aes-128-gcm",
                         help="ss-server encryption using to generate subscription url. Using aes-128-gcm as default.")
-    parser.add_argument("--key", type=str, default="0123456789abcdef",
+    parser.add_argument("-k", "--key", type=str, default="0123456789abcdef",
                         help="the AES key of network transportation encryption.")
-    parser.add_argument("--data-filename", type=str, default="ss-manager.csv",
+    parser.add_argument("-f", "--data-filename", type=str, default="ss-manager.csv",
                         help="name of the csv file used for persistent storage of data. "
                              "Using ss-manager.csv by default.")
-    parser.add_argument("--start-port", type=int, default=8001,
+    parser.add_argument("-sp", "--start-port", type=int, default=8001,
                         help="users' port pool start point (included). Using 8001 by default.")
-    parser.add_argument("--end-port", type=int, default=8501,
+    parser.add_argument("-ep", "--end-port", type=int, default=8501,
                         help="users' port pool end point (not included). Using 8501 by default.")
-    parser.add_argument("--default-monthly-traffic", type=int, default=100,
+    parser.add_argument("-dmt", "--default-monthly-traffic", type=int, default=100,
                         help="default monthly traffic (GB). Using 100 by default.")
-    parser.add_argument("--refresh-interval", type=int, default=30,
+    parser.add_argument("-ri", "--refresh-interval", type=int, default=30,
                         help="interval of writing data and check traffic usage. Using 30 by default.")
-    parser.add_argument("--ss-manager-address", type=str, default="/tmp/manager.sock",
+    parser.add_argument("-sma", "--ss-manager-address", type=str, default="/tmp/manager.sock",
                         help="address of ss-manager. Supporting net address and port or UDS. "
                              "Using /tmp/manager.sock by default.")
-    parser.add_argument("--api-address", type=str, default="/tmp/ss-manager-controller.sock",
+    parser.add_argument("-aa", "--api-address", type=str, default="/tmp/ss-manager-controller.sock",
                         help="address of controllers api service. Supporting net address and port or UDS. "
                              "Using /tmp/ss-manager-controller.sock by default.")
-    parser.add_argument("--reset-date", type=int, default=1,
+    parser.add_argument("-rd", "--reset-date", type=int, default=1,
                         help="traffic record refresh date. Using 1 by default.")
-    parser.add_argument("--reset-time", type=int, default=1,
+    parser.add_argument("-rt", "--reset-time", type=int, default=1,
                         help="traffic record refresh time. "
                              "Using 1 indicating record refreshed at 1:00 on reset date by default.")
 
